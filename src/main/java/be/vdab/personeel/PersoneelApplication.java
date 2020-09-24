ae12c0767b12b964d5fe9c21f5fc01ee735b499d
Moges <Moges.ash@gmail.com>
2020-09-24 14:40:20 +0200

    starting

---
+++ src/main/java/be/vdab/personeel/PersoneelApplication.java
@@ -0,0 +1,13 @@
+package be.vdab.personeel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersoneelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersoneelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/vdab/personeel/PersoneelApplicationTests.java
@@ -0,0 +1,13 @@
+package be.vdab.personeel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersoneelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
